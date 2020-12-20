98451bb0e5bbc6360b1dc5717d3ebae0c573a3d3
William <19631981+willy00@users.noreply.github.com>
2020-12-20 16:27:25 +0800

    Adding missing files

---
+++ CryptoApp/static/js/aes.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $(".action-encode").click(function () {
+    $(".action-encode").toggleClass("mid-box-action-active");
+    $(".action-decode").toggleClass("mid-box-action-active");
+    $(".process-btn").text("Encrypt");
+    $("#plaintext-area").val("");
+    $("#ciphertext-area").val("");
+    $(".box-title-right").text("Ciphertext");
+    $(".box-title-left").text("Plaintext");
+  });
+  $(".action-decode").click(function () {
+    $(".action-decode").toggleClass("mid-box-action-active");
+    $(".action-encode").toggleClass("mid-box-action-active");
+    $(".process-btn").text("Decrypt");
+    $("#plaintext-area").val("");
+    $("#ciphertext-area").val("");
+    $(".box-title-right").text("Plaintext");
+    $(".box-title-left").text("Ciphertext");
+  });
+  $(".process-btn").click(function () {
+    const selected_algo = $("select[name='algo-setting']").children("option:selected").val();
+    const bytes_size = selected_algo / 8;
+    const selected_mode = $("select[name='mode-setting']").children("option:selected").val();
+    const type = $(".process-btn").text().toLowerCase();
+    const text = $("#plaintext-area").val();
+    const key = $("input[name='text-setting-key']").val();
+    if (key.length !== bytes_size) {
+      window.alert("Invalid key length. It must be "+ bytes_size + " bytes");
+    } else {
+     let dat = {"type": type, "key": key, "text": text, "mode": selected_mode};
+      $.ajax({
+          url: "/api/crypto/aes/",
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(dat),
+          success: function(result) {
+            $("#ciphertext-area").val(result.output);
+          }
+      });
+    }
+  });
+});
